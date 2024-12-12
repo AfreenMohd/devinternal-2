e9513a588f2d06989a0f776ceb95bf89b4e0eb15
MOHAMMED AFREEN <147132205+AfreenMohd@users.noreply.github.com>
2024-12-12 10:40:42 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
+import java.io.*;
+import java.util.*;
 class Hello{
 public static void main(String[] args){
 int a=5,b=5;
 int res=a+b;
-System.out.prinltn("sum of a  and b is"+ res);
+System.out.println("sum of a  and b is"+ res);
 }
 }
